210ebef9a649972be1b46628510e2f756d63892a
PO <p.orlova18@mail.ru>
2022-12-03 21:05:46 +0300

    splash screen added

--- app/src/main/java/com/example/hantaton/MainActivity.java
+++ app/src/main/java/com/example/hantaton/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.hantaton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView schemasRV = findViewById(R.id.idRVCourse);
+        ArrayList<warn_schemas> warnSchemasArrayList = new ArrayList<warn_schemas>();
+
+
+        rv_adapter rv_adapter = new rv_adapter(warnSchemasArrayList, this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        schemasRV.setAdapter(rv_adapter);
+        schemasRV.setLayoutManager(linearLayoutManager);
     }
 }
